2b69e7e4411ef53d1d318397dd25d2b25cf7300a
Shekhar Patil <shekhar.patil@blazeclan.com>
2019-11-13 17:28:59 +0530

    middleware changes

--- middleware/index.js
+++ middleware/index.js
@@ -5,17 +5,17 @@ const secret = process.env.SECRET;
 const validateToken = (req, res, next) => {
    
 	// check header for the token
-	const token = req.headers.authorization;
+	let token = req.headers.authorization;
 
 	// decode token
 	if (token) {
 		// verifies secret and checks if the token is expired
-		jwt.verify(token, secret, (err, decoded) =>{      
+		jwt.verify(token, secret, (err) =>{      
 			if (err) {
-				res.status(404).json({ message: 'invalid token' });    
+				res.status(404).json({ message: 'Invalid token Provided' });    
 			} else {
-				// if everything is good, save to request for use in other routes
-				req.decoded = decoded;    
+				// // if everything is good, save to request for use in other routes
+				// req.decoded = decoded;    
 				next();
 			}
 		});
